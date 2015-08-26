07ae7a75c2bcddf75e0fa7ba85c3abf12fca2862
unknown <chlim2@697-X4-Tower.sf.k12.internal>
2015-08-26 10:25:13 -0700

    finished design

--- OriginalDesign.pde
+++ OriginalDesign.pde
@@ -7,7 +7,7 @@ void draw()
 {
 	
 }
-void mouseClicked()
+void mousePressed()
 {
 	dango();
 }
@@ -16,11 +16,11 @@ void dango()
 	int[] arrayR = {108, 235, 232, 116, 247, 89, 0, 204, 99, 12, 237, 4};
 	int[] arrayG = {252, 255, 181, 237, 190, 117, 153, 209, 68, 128, 112, 68};
 	int[] arrayB = {255, 105, 100, 102, 212, 255, 143, 48, 0, 27, 187, 125};
-	int fillColor = round(random(0,5));
+	int fillColor = (int)(Math.random()*6);
     fill(arrayR[fillColor], arrayG[fillColor], arrayB[fillColor]);
     stroke(arrayR[fillColor + 6], arrayG[fillColor + 6], arrayB[fillColor + 6]);
-	int x = 256;
-	int y = 256;
+	int x = mouseX;
+	int y = mouseY;
 	strokeWeight(4);
     ellipse(x, y, 100, 85);
     strokeWeight(1);
--- build-tmp/source/OriginalDesign.java
+++ build-tmp/source/OriginalDesign.java
@@ -23,7 +23,7 @@ public void draw()
 {
 	
 }
-public void mouseClicked()
+public void mousePressed()
 {
 	dango();
 }
@@ -32,11 +32,11 @@ public void dango()
 	int[] arrayR = {108, 235, 232, 116, 247, 89, 0, 204, 99, 12, 237, 4};
 	int[] arrayG = {252, 255, 181, 237, 190, 117, 153, 209, 68, 128, 112, 68};
 	int[] arrayB = {255, 105, 100, 102, 212, 255, 143, 48, 0, 27, 187, 125};
-	int fillColor = round(random(0,5));
+	int fillColor = (int)(Math.random()*6);
     fill(arrayR[fillColor], arrayG[fillColor], arrayB[fillColor]);
     stroke(arrayR[fillColor + 6], arrayG[fillColor + 6], arrayB[fillColor + 6]);
-	int x = 256;
-	int y = 256;
+	int x = mouseX;
+	int y = mouseY;
 	strokeWeight(4);
     ellipse(x, y, 100, 85);
     strokeWeight(1);
